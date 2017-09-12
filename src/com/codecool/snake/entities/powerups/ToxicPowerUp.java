9fd42f020fc829aff0d917f8494a4d3f30e933ea
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-09-12 15:13:50 +0200

    Implement toxic powerup class

--- src/com/codecool/snake/entities/powerups/ToxicPowerUp.java
+++ src/com/codecool/snake/entities/powerups/ToxicPowerUp.java
@@ -1,4 +1,31 @@
 package com.codecool.snake.entities.powerups;
 
-public class ToxicPowerUp {
+import com.codecool.snake.entities.GameEntity;
+import com.codecool.snake.Globals;
+import com.codecool.snake.entities.Interactable;
+import com.codecool.snake.entities.snakes.SnakeHead;
+import javafx.scene.layout.Pane;
+
+import java.util.Random;
+
+public class ToxicPowerUp extends GameEntity implements Interactable {
+
+    public ToxicPowerUp(Pane pane) {
+        super(pane);
+        setImage(Globals.powerupToxic);
+        pane.getChildren().add(this);
+
+        Random rnd = new Random();
+        setX(rnd.nextDouble() * Globals.WINDOW_WIDTH);
+        setY(rnd.nextDouble() * Globals.WINDOW_HEIGHT);
+    }
+
+    @Override
+    public void apply(SnakeHead snakeHead) {
+        snakeHead.takePart(3);
+        destroy();
+    }
+
+    @Override
+    public String getMessage() { return "Lost from your tail :'("; }
 }
